5b08c63d3552689794d6eae7fc260a85e8ff515d
Rodrigo <rodrigodemi@gmail.com>
2013-04-21 20:52:34 -0300

    pruebas

--- Lib-Comunicacion/Comunicacion/Comunicacion/ConversionUtil.cs
+++ Lib-Comunicacion/Comunicacion/Comunicacion/ConversionUtil.cs
@@ -24,5 +24,13 @@ namespace Comunicacion
             //System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
             //return new string(chars);
         }
+        public static string GetString(byte[] bytes, int bytesread)
+        {
+            return Encoding.UTF8.GetString(bytes, 0, bytesread);
+
+            //char[] chars = new char[bytes.Length / sizeof(char)];
+            //System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            //return new string(chars);
+        }
     }
 }
--- Lib-Comunicacion/Comunicacion/Pruebas/Program.cs
+++ Lib-Comunicacion/Comunicacion/Pruebas/Program.cs
@@ -2,6 +2,7 @@
 using Comunicacion;
 using System.Text;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Pruebas
 {
@@ -42,10 +43,62 @@ namespace Pruebas
             }
             Console.WriteLine("Done Multiple Payload");
 
+
+
             Console.WriteLine("|"+Settings.GetInstance().GetProperty("REQ01") +"|");
+            Console.WriteLine("Done reading property");
+
+
+
+            Consola();
+
+
+
 
             Console.WriteLine("Done");
             Console.ReadLine();
+        }
+
+
+
+        static public void Consola()
+        {
+            Stream inputStream = Console.OpenStandardInput();
+            byte[] bytes = new byte[10000];
+            Console.WriteLine("Escriba un coando del protocolo (se hace touppercase automaticco)");
+            Console.WriteLine("(Example: REQ0200015Hola soy Cli 1.");
+            Boolean end = false;
+            String buffer1  ="";
+            Boolean buffered = false;
+            while (!end)
+            {
+                
+                int outputLength = inputStream.Read(bytes, 0, 30);
+                Console.WriteLine("leido: " + outputLength);
+                //if(outputLength < 10 || buffered){
+                //    buffer1 +=ConversionUtil.GetString(bytes, outputLength);
+                //    buffered =true;
+                //    Console.WriteLine("buffer--->"+buffer1);
+                //}else{
+                    String inString = ConversionUtil.GetString(bytes, outputLength);
+                    //if(buffered){
+                    //    buffered =  false;
+                    //    inString=  buffer1 + inString;
+                    //    buffer1 ="";
+                    //}
+                      Console.WriteLine("inString--->"+inString);
+
+                //}
+               
+              
+                
+              // Enum.Parse(typeof(Command), 
+
+            }
+           
+            //char[] chars = Encoding.UTF7.GetChars(bytes, 0, outputLength);
+            Console.WriteLine("Decoded string:");
+            //Console.WriteLine(new string(chars));
         }
     }
 }
