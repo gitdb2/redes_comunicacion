c8339d48d3e78d9cb9fb84abfd653b7f593399a0
mauricio <karbunkle@gmail.com>
2013-04-30 14:59:17 -0300

    log4j

--- Chat/FormsCliente/Program.cs
+++ Chat/FormsCliente/Program.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Chat
 {
     static class Program
     {
+        private static log4net.ILog log;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            log4net.Config.XmlConfigurator.Configure(new FileInfo("log4net.config"));
+            log4net.GlobalContext.Properties["serverName"] = "Cliente";
+            log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            
+            log.Info("Inicio del Cliente");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
